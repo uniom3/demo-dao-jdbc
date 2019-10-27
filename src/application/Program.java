5a2ee9020fd64e9411ef9092f85ea9b6bb02de83
uniom3 <deivid.mendonca@hotmail.com>
2019-10-27 09:45:45 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,15 @@ System.out.println("\n===== TEST 3: seller findAll =====");
 	  Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(),400.0, department );
 	  sellerDao.insert(newSeller);
 	  System.out.println("Inserted! New id = "+ newSeller.getId());
+	
+	  System.out.println("\n===== TEST 5: seller update =====");
+	
+	  seller = sellerDao.findById(1);
+	  seller.setName("Martha Waine");
+	  sellerDao.update(seller);
+	  System.out.println("Update Complete");
+	  
+	  
 	}
 
 	
