783b4a241f807e16edffc758ade6b7efa9c7a196
uniom3 <deivid.mendonca@hotmail.com>
2019-10-27 09:38:47 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -32,6 +34,13 @@ System.out.println("\n===== TEST 3: seller findAll =====");
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n===== TEST 4: seller insert =====");
+	  Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(),400.0, department );
+	  sellerDao.insert(newSeller);
+	  System.out.println("Inserted! New id = "+ newSeller.getId());
 	}
 
+	
 }
+
+
